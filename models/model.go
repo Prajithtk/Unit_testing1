fbd3ff0817fa9ea511c79996afb7a1c8db7f0b7b
prajith <prajith01010@gmail.com>
2024-10-15 19:01:02 +0530

    json change

--- models/model.go
+++ models/model.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type User struct{
 	gorm.Model
-	Name string `gorm:"not null"`
-	Email string `gorm:"not null;unique"`
-	Password string `gorm:"not null"`
+	Name string `gorm:"not null" json:"name"`
+	Email string `gorm:"not null;unique" json:"email"`
+	Password string `gorm:"not null" json:"password"`
 }
